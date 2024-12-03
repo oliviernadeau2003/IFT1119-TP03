eda6d04626c06d2e9fc7a6bd1bed6ad5239f28e2
Olivier Nadeau <oliviernadeau2003@gmail.com>
2024-12-03 00:17:17 -0500

    .

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform Player;
+    public Transform MapLimiterTopLeft;
+    public Transform MapLimiterBottomRight;
+
+    Transform transform;
+
+    float cameraHalfWidth;
+    float cameraHalfHeight;
+
+    void Start()
+    {
+        transform = gameObject.GetComponent<Transform>();
+        Camera camera = gameObject.GetComponent<Camera>();
+
+        cameraHalfHeight = camera.orthographicSize;
+        cameraHalfWidth = camera.aspect * cameraHalfHeight;
+    }
+
+    void Update()
+    {
+        float xPosition, yPosition;
+
+        xPosition = Mathf.Clamp(
+                Player.position.x,
+                MapLimiterTopLeft.position.x + cameraHalfWidth,
+                MapLimiterBottomRight.position.x - cameraHalfWidth
+            );
+
+        yPosition = Mathf.Clamp(
+                Player.position.y,
+                MapLimiterBottomRight.position.y + cameraHalfHeight,
+                MapLimiterTopLeft.position.y - cameraHalfHeight
+            );
+
+        transform.position = new Vector3(xPosition, yPosition, transform.position.z);
+    }
+}
--- Assets/Scripts/RobotBoyController.cs
+++ Assets/Scripts/RobotBoyController.cs
@@ -2,20 +2,20 @@ using UnityEngine;
 
 public class RobotBoyController : MonoBehaviour
 {
-    public float speed = 50;
-    public float impulsion = 0;
+    public float speed = 35;
+    public float impulsion = 15;
 
     float movement = 0;
     bool isJumping;
     int nbJump;
     Rigidbody2D rigidBody2D;
-    SpriteRenderer spriceRenderer;
+    SpriteRenderer spriteRenderer;
     Animator animator;
 
     void Start()
     {
         rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
-        spriceRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         animator = gameObject.GetComponent<Animator>();
 
         movement = 0;
@@ -26,11 +26,36 @@ public class RobotBoyController : MonoBehaviour
     void Update()
     {
         movement = Input.GetAxis("Horizontal");
+        animator.SetFloat("State", Mathf.Abs(movement));
+
+        // X Rotation
+        if (movement < 0)
+            spriteRenderer.flipX = true;
+        else
+            spriteRenderer.flipX = false;
+
+        // Jump
+        if (Input.GetKeyDown(KeyCode.W) && nbJump < 2)
+        {
+            isJumping = true;
+            animator.SetTrigger("Jump");
+            nbJump++;
+        }
     }
 
     private void FixedUpdate()
     {
         rigidBody2D.AddRelativeForce(Vector2.right * speed * movement);
-        
+
+        if (isJumping)
+        {
+            rigidBody2D.AddRelativeForce(Vector2.up * impulsion, ForceMode2D.Impulse);
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        nbJump = 0;
     }
 }
